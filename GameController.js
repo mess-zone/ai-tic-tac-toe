f2b7d0ca6686bae0e769782b40634e38e8b542b8
Gilmar Andrade <gilmar-andrade@outlook.com>
2021-12-31 19:38:54 -0300

    human hover efect

--- GameController.js
+++ GameController.js
@@ -1,43 +1,54 @@
 import Board from './Board.js';
 import ComputerPlayer from './ComputerPlayer.js';
+import HumanPlayer from './HumanPlayer.js';
 
 export default class GameController {
     constructor() {
         this.boardSize = 3; // 3x3
         this.board = new Board(this.boardSize);
         
-        this.players = [ new ComputerPlayer('P1', 'x'), new ComputerPlayer('P2', 'o') ];
+        this.players = [ new HumanPlayer('P1', 'x'), new ComputerPlayer('P2', 'o') ];
         this.currentPlayer = 0;
         this.winner = null;
         this.status = null;
+
+        this.boardScreenEl = document.getElementById('board-screen');
+        this.scoreEl = this.boardScreenEl.querySelector('#score');
+        this.hintEl = this.boardScreenEl.querySelector('#hint');
+        this.boardEl = this.boardScreenEl.querySelector('#board');
     }
 
     switchPlayer() {
         this.currentPlayer = (this.currentPlayer + 1) % this.players.length;
+        if(this.players[this.currentPlayer] instanceof HumanPlayer) {
+            this.boardEl.classList.add('board--human-turn');
+        } else {
+            this.boardEl.classList.remove('board--human-turn');
+        }
     }
 
     async startGame() {
         this.currentPlayer = 0;
+
         this.winner = null;
         this.status = null;
         this.board.reset();
 
-        const boardScreenEl = document.getElementById('board-screen');
-        const scoreEl = boardScreenEl.querySelector('#score');
-        const hintEl = boardScreenEl.querySelector('#hint');
-
-        scoreEl.innerHTML = '';
+        this.scoreEl.innerHTML = '';
         this.players.forEach(player => {
             const h1 = document.createElement('h1');
             h1.innerHTML = `${player.name} (${player.symbol}): 0 points`;
-            scoreEl.appendChild(h1);
+            this.scoreEl.appendChild(h1);
         });
 
+        if(this.players[this.currentPlayer] instanceof HumanPlayer) {
+            this.boardEl.classList.add('board--human-turn');
+        }
 
 
         //move
         while(this.status == null) {
-            hintEl.innerHTML = `${this.players[this.currentPlayer].name}: your turn!`;
+            this.hintEl.innerHTML = `${this.players[this.currentPlayer].name}: your turn!`;
             
             const c = await this.players[this.currentPlayer].move(this.board.cells, this.board.size);
             console.log(c)
