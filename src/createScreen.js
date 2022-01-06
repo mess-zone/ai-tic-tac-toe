249b14867f8add996d22904dd48fa44de492a5ca
Gilmar Andrade <gilmar-andrade@outlook.com>
2022-01-06 10:05:49 -0300

    update score

--- src/createScreen.js
+++ src/createScreen.js
@@ -1,5 +1,9 @@
+import { Symbols } from "./createGame.js";
+
 export default function createScreen(window) {
     let nodes = {};
+
+    let state = {};
 
     const observers = [];
 
@@ -31,11 +35,11 @@ export default function createScreen(window) {
         window.addEventListener('resize', handleResize);
 
         nodes.startScreenEl.querySelector('form').addEventListener('submit', configurePlayers);
+
+        nodes.roundScreenEl.addEventListener("animationend", showBoardScreen);
     }
 
     function handleResize(e) {
-        console.log(e)
-        console.log(nodes.boardContainerEl.offsetWidth, nodes.boardContainerEl.offsetHeight)
         const side = nodes.boardContainerEl.offsetWidth <= nodes.boardContainerEl.offsetHeight ? nodes.boardContainerEl.offsetWidth : nodes.boardContainerEl.offsetHeight;
 
         nodes.boardEl.style.width = side + 'px';
@@ -48,20 +52,13 @@ export default function createScreen(window) {
         if(command.id == 'SETUP') {
             showStartScreen();
         } else if(command.id == 'START_ROUND') {
+            state = command.state;
             showRoundScreen();
         }
     }
 
     function showStartScreen() {
         nodes.startScreenEl.classList.add('screen--show');
-    }
-
-    function showRoundScreen() {
-        nodes.endRoundScreenEl.classList.remove('screen--show');
-        nodes.endGameScreenEl.classList.remove('screen--show');
-        nodes.roundScreenEl.querySelector('h1').innerText = `Round ?/??`;
-        nodes.roundScreenEl.classList.add('screen--show');
-        nodes.roundScreenEl.classList.add('animating');
     }
 
     function configurePlayers(e) {
@@ -78,7 +75,6 @@ export default function createScreen(window) {
 
         console.log('configure players', player1, player2);
 
-        // TODO notify game
         notifyAll({ id: 'SETUP', player1, player2 });
         // this.players = [];            
         // this.players.push((player1.type == 'human') ? new HumanPlayer(player1.name, 'x') : new ComputerPlayer(player1.name, 'x'));
@@ -91,6 +87,56 @@ export default function createScreen(window) {
         // ];
         
         // startNewRound();
+    }
+
+    function showRoundScreen() {
+        nodes.endRoundScreenEl.classList.remove('screen--show');
+        nodes.endGameScreenEl.classList.remove('screen--show');
+        nodes.roundScreenEl.querySelector('h1').innerText = `Round ${state.currentRound.round + 1}/${state.maxRounds}`;
+        nodes.roundScreenEl.classList.add('screen--show');
+        nodes.roundScreenEl.classList.add('animating');
+    }
+
+    function showBoardScreen() {
+        console.log('[screen] show board screen')
+        nodes.startScreenEl.classList.remove('screen--show');
+        nodes.boardScreenEl.classList.add('screen--show');
+        handleResize();
+        updateScore();
+        drawBoard();
+        nodes.roundScreenEl.classList.remove('screen--show');
+        nodes.roundScreenEl.classList.remove('animating');
+    }
+
+    function drawBoard() {
+        for(let i = 0; i < state.board.cells.length; i++) {
+            if(state.board.cells[i] == '') {
+                nodes.cellsEl[i].classList.add('board__cell--empty');
+            } else {
+                nodes.cellsEl[i].classList.remove('board__cell--empty');
+                nodes.cellsEl[i].classList.add(`board__cell--${state.board.cells[i]}`);
+            }                   
+        }
+    }
+
+    function updateScore() {
+        nodes.scoreEl.innerHTML = '';
+
+        const roundCounterEl = document.createElement('h1');
+        roundCounterEl.innerHTML = `Round: ${state.currentRound.round + 1}/${state.maxRounds}`;
+        nodes.scoreEl.appendChild(roundCounterEl);
+
+        const xScoreEl = document.createElement('h1');
+        xScoreEl.innerHTML = `${Symbols.X}: ${state.scores.reduce((acc, val) => (val.winner == Symbols.X ? acc + 1 :  acc), 0 )}`;
+        nodes.scoreEl.appendChild(xScoreEl);
+
+        const oScoreEl = document.createElement('h1');
+        oScoreEl.innerHTML = `${Symbols.O}: ${state.scores.reduce((acc, val) => (val.winner == Symbols.O ? acc + 1 :  acc), 0 )}`;
+        nodes.scoreEl.appendChild(oScoreEl);
+
+        const drawScoreEl = document.createElement('h1');
+        drawScoreEl.innerHTML = `Draw: ${state.scores.reduce((acc, val) => (val.winner == 'Draw' ? acc + 1 :  acc), 0 )}`;
+        nodes.scoreEl.appendChild(drawScoreEl);
     }
     
     return {
