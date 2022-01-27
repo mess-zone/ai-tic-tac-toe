294ce5f4c8f4b48491a720037493af9c5efa9999
Gilmar Andrade <gilmar-andrade@outlook.com>
2022-01-27 18:46:24 -0300

    command END ROUND

--- src/ui/createCommands.js
+++ src/ui/createCommands.js
@@ -60,9 +60,58 @@ export default function createCommands(viewsController) {
         viewsController.switchTurn(switchTurnModel);
     }
 
+    function END_ROUND(command) {
+        console.log('[ui] END ROUND');
+        const state = {...command.state};
+
+        const updateScoreModel = {
+            round: {
+                currentRound: state.currentRound.round + 1,
+                maxRounds: state.maxRounds,
+            },
+            X: {
+                name: state.players[0].name,
+                symbol: state.players[0].symbol,
+                points: state.scores.reduce((acc, val) => (val.winner == Symbols.X ? acc + 1 :  acc), 0 ),
+            },
+            O: {
+                name: state.players[1].name,
+                symbol: state.players[1].symbol,
+                points: state.scores.reduce((acc, val) => (val.winner == Symbols.O ? acc + 1 :  acc), 0 ),
+            },
+            draws: {
+                name: 'Draws',
+                symbol: '',
+                points: state.scores.reduce((acc, val) => (val.winner == 'Draw' ? acc + 1 :  acc), 0 ),
+            },
+        };
+
+        viewsController.updateScore(updateScoreModel);
+
+        const drawBoardModel = {
+            boardCells: state.board.cells,
+            winnerCombination: state.scores[state.currentRound.round]?.combination,
+        };
+        viewsController.drawBoard(drawBoardModel);
+
+        const switchTurnModel = {
+            isXTurn: state.players[state.currentRound.currentPlayer].symbol === Symbols.X,
+            isOTurn: state.players[state.currentRound.currentPlayer].symbol === Symbols.O,
+            isHumanTurn: state.players[state.currentRound.currentPlayer].type === PlayerTypes.HUMAN,
+            hintText: state.currentRound.statusRound == RoundStatus.PLAYING ? `${state.players[state.currentRound.currentPlayer].name}: your turn!` : '',
+        };
+        viewsController.switchTurn(switchTurnModel);
+
+        const endRoundScreenModel = {
+            text: state.currentRound.statusRound == RoundStatus.WIN ? `${state.players[state.currentRound.currentPlayer].name} won!` : 'Draw!',
+        };
+        viewsController.showEndRoundScreen(endRoundScreenModel);
+    }
+
     return {
         SETUP,
         START_ROUND,
         UPDATE_BOARD,
+        END_ROUND,
     }
 }
--- src/ui/createScreen.js
+++ src/ui/createScreen.js
@@ -27,50 +27,7 @@ export default function createScreen(viewsController, commands) {
             commands.UPDATE_BOARD(command);
         } else if(command.id == 'END_ROUND') {
             state = {...command.state};
-            console.log('[screen] END ROUND', state)
-
-            const updateScoreModel = {
-                round: {
-                    currentRound: state.currentRound.round + 1,
-                    maxRounds: state.maxRounds,
-                },
-                X: {
-                    name: state.players[0].name,
-                    symbol: state.players[0].symbol,
-                    points: state.scores.reduce((acc, val) => (val.winner == Symbols.X ? acc + 1 :  acc), 0 ),
-                },
-                O: {
-                    name: state.players[1].name,
-                    symbol: state.players[1].symbol,
-                    points: state.scores.reduce((acc, val) => (val.winner == Symbols.O ? acc + 1 :  acc), 0 ),
-                },
-                draws: {
-                    name: 'Draws',
-                    symbol: '',
-                    points: state.scores.reduce((acc, val) => (val.winner == 'Draw' ? acc + 1 :  acc), 0 ),
-                },
-            };
-
-            viewsController.updateScore(updateScoreModel);
-
-            const drawBoardModel = {
-                boardCells: state.board.cells,
-                winnerCombination: state.scores[state.currentRound.round]?.combination,
-            };
-            viewsController.drawBoard(drawBoardModel);
-
-            const switchTurnModel = {
-                isXTurn: state.players[state.currentRound.currentPlayer].symbol === Symbols.X,
-                isOTurn: state.players[state.currentRound.currentPlayer].symbol === Symbols.O,
-                isHumanTurn: state.players[state.currentRound.currentPlayer].type === PlayerTypes.HUMAN,
-                hintText: state.currentRound.statusRound == RoundStatus.PLAYING ? `${state.players[state.currentRound.currentPlayer].name}: your turn!` : '',
-            };
-            viewsController.switchTurn(switchTurnModel);
-
-            const endRoundScreenModel = {
-                text: state.currentRound.statusRound == RoundStatus.WIN ? `${state.players[state.currentRound.currentPlayer].name} won!` : 'Draw!',
-            };
-            viewsController.showEndRoundScreen(endRoundScreenModel);
+            commands.END_ROUND(command);
         } else if(command.id == 'END_GAME') {
             state = {...command.state};
             console.log('[screen] END GAME', state);
