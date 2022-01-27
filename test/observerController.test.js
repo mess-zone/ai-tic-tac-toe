0c4bf1b9adfb7dbbd7068245b685a9a3098516c6
Gilmar Andrade <gilmar-andrade@outlook.com>
2022-01-27 09:58:37 -0300

    tests observer.notifyAll

--- test/observerController.test.js
+++ test/observerController.test.js
@@ -24,4 +24,41 @@ describe('observerController', function() {
             expect(() => { sut.subscribe('invalid_param') }).to.throw();
         });
     });
+
+    describe('#notifyAll', function() {
+        it('Should notify all subscribers when receive a valid command', function() {
+            const sut = createObserverController();
+
+            let counter = 0;
+            const validCallback = function() {
+                counter++;
+            };
+
+            sut.subscribe(validCallback);
+            sut.subscribe(validCallback);
+            sut.subscribe(validCallback);
+
+            const valid_command = {};
+            sut.notifyAll(valid_command);
+
+            expect(counter).to.equal(3);
+        });
+
+        it('Should notify all subscribers even if receive no param', function() {
+            const sut = createObserverController();
+
+            let counter = 0;
+            const validCallback = function() {
+                counter++;
+            };
+
+            sut.subscribe(validCallback);
+            sut.subscribe(validCallback);
+            sut.subscribe(validCallback);
+
+            sut.notifyAll();
+
+            expect(counter).to.equal(3);
+        });
+    });
 });
