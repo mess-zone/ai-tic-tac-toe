71ee0150aaf89e454ffadaef1b073b227110073b
Gilmar Andrade <gilmar-andrade@outlook.com>
2022-01-29 10:41:06 -0300

    cypress verify board cells

--- cypress/integration/start-screen.spec.js
+++ cypress/integration/start-screen.spec.js
@@ -53,7 +53,32 @@ describe('Start Game', () => {
             .next()
             .should('have.text', 'Draws: 0')
 
-        // TODO verificar se a board está vazia e se é a vez do player 1
+        // verificar se a board está vazia e se é a vez do player 1
+        cy.get('#board')
+            .should('have.class', 'board--human-turn')
+            .should('have.class', 'turn--X')
+
+        cy.get('.board__cell')
+            .should('have.class', 'board__cell--empty')
+            .should('have.length', 9)
+            .first()
+            .should('have.attr', 'data-i', 0)
+            .next()
+            .should('have.attr', 'data-i', 1)
+            .next()
+            .should('have.attr', 'data-i', 2)
+            .next()
+            .should('have.attr', 'data-i', 3)
+            .next()
+            .should('have.attr', 'data-i', 4)
+            .next()
+            .should('have.attr', 'data-i', 5)
+            .next()
+            .should('have.attr', 'data-i', 6)
+            .next()
+            .should('have.attr', 'data-i', 7)
+            .next()
+            .should('have.attr', 'data-i', 8)
 
     })
 })
