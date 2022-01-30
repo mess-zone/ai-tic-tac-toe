3f15c946f3739d7655c52d5b9549c1da283e76fa
Gilmar Andrade <gilmar-andrade@outlook.com>
2022-01-30 17:08:24 -0300

    cypress: refactor board user interaction specs

--- cypress/integration/board-user-interactions.spec.js
+++ cypress/integration/board-user-interactions.spec.js
@@ -42,53 +42,17 @@ describe('Board user interactions', () => {
 
     it('Every empty cell must be clickable if the current player is human', () => {
 
+        const cells = [ 0, 1, 2, 3, 4, 6, 5, 8, 7]
+
         cy.get('[data-board-screen__board]')
             .should('have.class', 'board--human-turn')
 
-        cy.get('[data-board-screen__cell=0]')
-            .should('have.class', 'board__cell--empty')
-            .click()
-            .should('not.have.class', 'board__cell--empty')
-
-        cy.get('[data-board-screen__cell=1]')
-            .should('have.class', 'board__cell--empty')
-            .click()
-            .should('not.have.class', 'board__cell--empty')
-
-        cy.get('[data-board-screen__cell=2]')
-            .should('have.class', 'board__cell--empty')
-            .click()
-            .should('not.have.class', 'board__cell--empty')
-
-        cy.get('[data-board-screen__cell=3]')
-            .should('have.class', 'board__cell--empty')
-            .click()
-            .should('not.have.class', 'board__cell--empty')
-
-        cy.get('[data-board-screen__cell=4]')
-            .should('have.class', 'board__cell--empty')
-            .click()
-            .should('not.have.class', 'board__cell--empty')
-
-        cy.get('[data-board-screen__cell=6]')
-            .should('have.class', 'board__cell--empty')
-            .click()
-            .should('not.have.class', 'board__cell--empty')
-
-        cy.get('[data-board-screen__cell=5]')
-            .should('have.class', 'board__cell--empty')
-            .click()
-            .should('not.have.class', 'board__cell--empty')
-
-        cy.get('[data-board-screen__cell=8]')
-            .should('have.class', 'board__cell--empty')
-            .click()
-            .should('not.have.class', 'board__cell--empty')
-
-        cy.get('[data-board-screen__cell=7]')
-            .should('have.class', 'board__cell--empty')
-            .click()
-            .should('not.have.class', 'board__cell--empty')
+        cells.forEach(cell => {
+            cy.get(`[data-board-screen__cell=${cell}]`)
+                .should('have.class', 'board__cell--empty')
+                .click()
+                .should('not.have.class', 'board__cell--empty')
+        });
 
     })
 
@@ -117,48 +81,41 @@ describe('Board user interactions', () => {
     
     it('Should adapt board responsiveness if user resizes window', () => {
         // board size should always be the minor side of container
-        
-        // se largura container menor igual que altura container
-        cy.viewport(720, 1280)
 
-        cy.get('[data-board-screen__board-container]').invoke('width').then((containerWidth) => {
-            cy.get('[data-board-screen__board-container]').invoke('height').then((containerHeight) => {
-                const boardSize = Math.min(containerWidth, containerHeight);
+        const viewports = [
+            {
+                // se largura container menor igual que altura container
+                width: 720,
+                height: 1280
+            },
+            {
+                // se largura container igual que altura container
+                width: 490,
+                height: 700
+            },
+            {
+                // se largura container maior que altura container
+                width: 1280,
+                height: 720
+            }
+        ]
 
-                cy.get('[data-board-screen__board]')
-                    .should('have.css', 'width', boardSize + 'px')
-                    .should('have.css', 'height', boardSize + 'px')
+        viewports.forEach(viewport => {
+            cy.viewport(viewport.width, viewport.height)
+    
+            cy.get('[data-board-screen__board-container]').invoke('width').then((containerWidth) => {
+                cy.get('[data-board-screen__board-container]').invoke('height').then((containerHeight) => {
+                    const boardSize = Math.min(containerWidth, containerHeight);
+    
+                    cy.get('[data-board-screen__board]')
+                        .should('have.css', 'width', boardSize + 'px')
+                        .should('have.css', 'height', boardSize + 'px')
+                });
+    
             });
-
         });
 
-        // se largura container igual que altura container
-        cy.viewport(490, 700)
 
-        cy.get('[data-board-screen__board-container]').invoke('width').then((containerWidth) => {
-            cy.get('[data-board-screen__board-container]').invoke('height').then((containerHeight) => {
-                const boardSize = (containerWidth <= containerHeight) ? containerWidth : containerHeight;
-
-                cy.get('[data-board-screen__board]')
-                    .should('have.css', 'width', boardSize + 'px')
-                    .should('have.css', 'height', boardSize + 'px')
-            });
-
-        });
-
-        // se largura container maior que altura container
-        cy.viewport(1280, 720)
-
-        cy.get('[data-board-screen__board-container]').invoke('width').then((containerWidth) => {
-            cy.get('[data-board-screen__board-container]').invoke('height').then((containerHeight) => {
-                const boardSize = (containerWidth <= containerHeight) ? containerWidth : containerHeight;
-
-                cy.get('[data-board-screen__board]')
-                    .should('have.css', 'width', boardSize + 'px')
-                    .should('have.css', 'height', boardSize + 'px')
-            });
-
-        });
     })
 
 })
